cee2327167330a5a5a4ebb79579c4461852f57d2
Weber588 <idonthavetotellyoumyname@gmail.com>
2025-04-04 20:39:22 +1100

    Made Leap directional to allow accuracy

--- src/main/java/wbs/wandcraft/spell/definitions/LeapSpell.java
+++ src/main/java/wbs/wandcraft/spell/definitions/LeapSpell.java
@@ -3,23 +3,28 @@ package wbs.wandcraft.spell.definitions;
 import org.bukkit.Particle;
 import org.bukkit.entity.Player;
 import org.bukkit.scheduler.BukkitRunnable;
-import wbs.utils.util.entities.WbsEntityUtil;
 import wbs.wandcraft.WbsWandcraft;
 import wbs.wandcraft.spell.definitions.extensions.CastContext;
 import wbs.wandcraft.spell.definitions.extensions.CastableSpell;
+import wbs.wandcraft.spell.definitions.extensions.DirectionalSpell;
 import wbs.wandcraft.spell.definitions.extensions.SpeedSpell;
 
-public class LeapSpell extends SpellDefinition implements CastableSpell, SpeedSpell {
+public class LeapSpell extends SpellDefinition implements CastableSpell, DirectionalSpell, SpeedSpell {
     public LeapSpell() {
         super("leap");
         addAttribute(COOLDOWN, 5);
     }
 
     @Override
+    public double getMaxAngle() {
+        return 5;
+    }
+
+    @Override
     public void cast(CastContext context) {
         Player player = context.player();
         player.getWorld().spawnParticle(Particle.INSTANT_EFFECT, player.getLocation(), 160, 0, 0, 0, 0.5);
-        WbsEntityUtil.push(player, context.instance().getAttribute(SPEED));
+        player.setVelocity(getDirection(context, context.instance().getAttribute(SPEED)));
 
         new BukkitRunnable() {
             int escape = 0;
