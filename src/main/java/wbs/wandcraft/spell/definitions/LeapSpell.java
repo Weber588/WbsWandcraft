c6913df98aff5d43fc421076790a867a2f5ee556
Weber588 <idonthavetotellyoumyname@gmail.com>
2025-06-03 15:26:11 +1000

    Fix units on LeapSpell

--- src/main/java/wbs/wandcraft/spell/definitions/LeapSpell.java
+++ src/main/java/wbs/wandcraft/spell/definitions/LeapSpell.java
@@ -22,7 +22,10 @@ public class LeapSpell extends SpellDefinition implements CastableSpell, Directi
     public void cast(CastContext context) {
         Player player = context.player();
         player.getWorld().spawnParticle(Particle.INSTANT_EFFECT, player.getLocation(), 160, 0, 0, 0, 0.5);
-        player.setVelocity(getDirection(context, context.instance().getAttribute(SPEED)));
+        // Entity velocity is stored in blocks per SECOND, not tick
+        player.setVelocity(getDirection(context, context.instance().getAttribute(SPEED) * 20));
+
+        player.setFallDistance(0);
 
         new BukkitRunnable() {
             int escape = 0;
@@ -31,8 +34,6 @@ public class LeapSpell extends SpellDefinition implements CastableSpell, Directi
                 player.getWorld().spawnParticle(Particle.INSTANT_EFFECT, player.getLocation().add(0, 1, 0), 10, 0.4, 1, 0.4, 0);
 
                 escape++;
-
-                player.setFallDistance(0);
 
                 //noinspection deprecation
                 if (escape > 1000 || !player.isOnline() || player.isFlying() || (player.isOnGround() && escape >= 5)) {
